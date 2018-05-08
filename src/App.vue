af395c28b8401ffee440995cc8509f44d0639dc3
PUGE <100284685@qq.com>
2018-05-08 15:52:30 +0800

    完成第一个版本

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,247 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="slideshow">
+    <div class="entire-content">
+      <div v-if="tableData" class="content-carrousel" :class="{rotate: enlargeItem === 0}">
+        <figure class="shadow" @click="enlarge(1)" :class="{enlarge: enlargeItem === 1}">
+          <Excl :data="tableData"></Excl>
+        </figure>
+        <figure class="shadow" @click="enlarge(2)" :class="{enlarge: enlargeItem === 2}">
+          <Excl :data="tableData2"></Excl>
+        </figure>
+        <figure class="shadow" @click="enlarge(3)" :class="{enlarge: enlargeItem === 3}">
+          <Excl :data="tableData3"></Excl>
+        </figure>
+      </div>
+    </div>
+    <div class="close" v-show="enlargeItem !== 0" @click="enlargeItem = 0">x</div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Excl from './components/Excl.vue'
 export default {
   name: 'app',
+  data () {
+    return {
+      isPaused: false,
+      enlargeItem: 0,
+      tableData: null,
+      tableData2: null,
+      tableData3: null
+    }
+  },
   components: {
-    HelloWorld
+    Excl
+  },
+  methods: {
+    enlarge (key) {
+      this.enlargeItem = key
+    },
+    // 深拷贝
+    deepClone (obj) {
+      if (obj == null || typeof obj !== 'object') {
+        return obj
+      }
+      switch (Object.prototype.toString.call(obj)) {
+        case '[object Array]': {
+          const result = new Array(obj.length)
+          for (let i = 0; i < result.length; ++i) {
+            result[i] = this.deepClone(obj[i])
+          }
+          return result
+        }
+        // Object.prototype.toString.call(new XxxError) returns '[object Error]'
+        case '[object Error]': {
+          const result = new obj.constructor(obj.message)
+          result.stack = obj.stack // hack...
+          return result
+        }
+        case '[object Date]':
+        case '[object RegExp]':
+        case '[object Int8Array]':
+        case '[object Uint8Array]':
+        case '[object Uint8ClampedArray]':
+        case '[object Int16Array]':
+        case '[object Uint16Array]':
+        case '[object Int32Array]':
+        case '[object Uint32Array]':
+        case '[object Float32Array]':
+        case '[object Float64Array]':
+        case '[object Map]':
+        case '[object Set]':
+          return new obj.constructor(obj)
+        case '[object Object]': {
+          const keys = Object.keys(obj)
+          const result = {}
+          for (let i = 0; i < keys.length; ++i) {
+            const key = keys[i]
+            result[key] = this.deepClone(obj[key])
+          }
+          return result
+        }
+        default: {
+          throw new Error("Unable to copy obj! Its type isn't supported.")
+        }
+      }
+    }
+  },
+  created () {
+    const sendData = {
+      type: 'id',
+      data: '5'
+    }
+    // 发起post请求
+    const fetchConfig = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      credentials: 'credentials',
+      cache: 'default',
+      body: JSON.stringify(sendData)
+    }
+    fetch('http://192.168.1.105/index/index/select_data/', fetchConfig).then((res) => {
+      if (res.ok) { // 请求成功执行回掉
+        res.json().then((data) => {
+          data.data = data.data.replace(/NULL/g, '')
+          data.data = JSON.parse(data.data)
+          this.tableData = this.deepClone(data)
+          this.tableData2 = this.deepClone(data)
+          this.tableData3 = this.deepClone(data)
+        })
+      }
+    }, (e) => {
+      console.error('[POST]请求失败！', e)
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    max-width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+    transform: translate3d(0, 0, 0);
+  }
+
+  #slideshow {
+    margin: 0 auto;
+    height: 350px;
+    width: 100%;
+    height: 100%;
+    background-color: #6adecd;
+    box-sizing: border-box;
+  }
+
+  .slideshow-title {
+    font-family: 'Allerta Stencil';
+    font-size: 62px;
+    color: #fff;
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 25%;
+    letter-spacing: 3px;
+    font-weight: 300;
+  }
+
+  .sub-heading {
+    padding-top: 50px;
+    font-size: 18px;
+  }
+  .sub-heading-two { font-size: 15px; }
+  .sub-heading-three { font-size: 13px; }
+  .sub-heading-four { font-size: 11px; }
+  .sub-heading-five { font-size: 9px; }
+  .sub-heading-six { font-size: 7px; }
+  .sub-heading-seven { font-size: 5px; }
+  .sub-heading-eight { font-size: 3px; }
+  .sub-heading-nine { font-size: 1px; }
+
+  .entire-content {
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    perspective: 1000px;
+    position: relative;
+  }
+
+  .content-carrousel {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    float: right;
+  }
+  .rotate {
+    /* 调节动画快慢 */
+    animation: rotar 60s infinite linear;
+    transform-style: preserve-3d;
+  }
+
+  .entire-content .content-carrousel .enlarge {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    transform: none;
+    z-index: 999;
+  }
+  .content-carrousel .enlarge .title {
+    height: 40px;
+    line-height: 40px;
+  }
+  .content-carrousel:hover {
+    animation-play-state: paused;
+  }
+
+  .content-carrousel figure {
+    width: 600px;
+    height: 410px;
+    z-index: 1;
+    transition: all 500ms;
+    border: 1px solid #3b444b;
+    overflow: hidden;
+    position: absolute;
+  }
+
+  .content-carrousel figure:nth-child(1) { transform: rotateY(0deg) translateZ(300px); }
+  .content-carrousel figure:nth-child(2) { transform: rotateY(120deg) translateZ(300px); }
+  .content-carrousel figure:nth-child(3) { transform: rotateY(240deg) translateZ(300px); }
+  .shadow {
+    cursor: pointer;
+    position: absolute;
+    box-shadow: 0px 0px 20px 0px #000;
+    border-radius: 1px;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+  }
+  .close {
+    position: fixed;
+    right: 0;
+    top: 0;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    background-color: khaki;
+    text-align: center;
+    line-height: 40px;
+    font-size: 2rem;
+  }
+  @keyframes rotar {
+    from {
+      transform: rotateY(0deg);
+    } to {
+      transform: rotateY(360deg);
+    }
+  }
 </style>
---
+++ src/components/Excl.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="show">
+    <div class="title">{{data.name}}</div>
+    <table v-if="tableData" border="0">
+      <tr v-for="(item, ind) in tableData" :key="ind">
+        <td v-for="(data, index) in item" :key="index" :rowspan="data.rowspan" :colspan="data.colspan">{{data.value}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'excl',
+  props: {
+    data: Object
+  },
+  data () {
+    return {
+      tableData: null
+    }
+  },
+  created () {
+    // 将数据转换格式
+    let dataCopy = this.deepClone(this.data.data)
+    let format = JSON.parse(this.data.format)
+    let newData = []
+    // console.log(dataCopy)
+    for (const ind in dataCopy) {
+      // console.log(ind)
+      // console.log(dataCopy[ind])
+      let columnData = this.deepClone(dataCopy[ind])
+      // console.log(columnData)
+      for (const key in columnData) {
+        // console.log(columnData[key])
+        const value = this.deepClone(columnData[key])
+        // console.log(value)
+        columnData[key] = {
+          value,
+          colspan: 1,
+          rowspan: 1
+        }
+      }
+      // console.log(columnData)
+      dataCopy[ind] = columnData
+    }
+    // 进行行/列合并处理
+    for (const key in format) {
+      const formatItem = format[key]
+      // console.log(formatItem)
+      // 拆分出合并的起始和末尾
+      const first = formatItem.split(':')
+      const start = first[0]
+      const end = first[1]
+      // 将起始和末尾再分割出行列
+      const startCoordinate = start.split('-')
+      const startRow = startCoordinate[0]
+      const startColumn = startCoordinate[1]
+      const endCoordinate = end.split('-')
+      const endRow = endCoordinate[0]
+      const endColumn = endCoordinate[1]
+      // 根据开始和结束行坐标是否相同 判断是行合并还是列合并
+      if (startColumn === endColumn) {
+        // 行合并
+        // console.log(startRow, endRow)
+        // console.log(this.betwon(startRow, endRow, 1, 1))
+        const blockList = this.betwon(startRow, endRow, 1, 1)
+        // 进行行合并
+        // 减去1的原因是数组是由0键值开始
+        // 加上1的原因是数组中没有包含开始项
+        dataCopy[startColumn - 1][startRow].colspan = blockList.length + 1
+        // 删除空行
+        blockList.forEach(element => {
+          delete dataCopy[startColumn - 1][element]
+        })
+      } else {
+        console.log(dataCopy[startColumn - 1][startRow].rowspan)
+        dataCopy[startColumn - 1][startRow].rowspan = parseInt(endColumn) - parseInt(startColumn) + 1
+        for (let i = parseInt(startColumn); i < parseInt(endColumn); i++) {
+          delete dataCopy[i][startRow]
+        }
+      }
+    }
+    this.tableData = dataCopy
+    console.log(this.tableData)
+  },
+  methods: {
+    // 深拷贝
+    deepClone (obj) {
+      if (obj == null || typeof obj !== 'object') {
+        return obj
+      }
+      switch (Object.prototype.toString.call(obj)) {
+        case '[object Array]': {
+          const result = new Array(obj.length)
+          for (let i = 0; i < result.length; ++i) {
+            result[i] = this.deepClone(obj[i])
+          }
+          return result
+        }
+        // Object.prototype.toString.call(new XxxError) returns '[object Error]'
+        case '[object Error]': {
+          const result = new obj.constructor(obj.message)
+          result.stack = obj.stack // hack...
+          return result
+        }
+        case '[object Date]':
+        case '[object RegExp]':
+        case '[object Int8Array]':
+        case '[object Uint8Array]':
+        case '[object Uint8ClampedArray]':
+        case '[object Int16Array]':
+        case '[object Uint16Array]':
+        case '[object Int32Array]':
+        case '[object Uint32Array]':
+        case '[object Float32Array]':
+        case '[object Float64Array]':
+        case '[object Map]':
+        case '[object Set]':
+          return new obj.constructor(obj)
+        case '[object Object]': {
+          const keys = Object.keys(obj)
+          const result = {}
+          for (let i = 0; i < keys.length; ++i) {
+            const key = keys[i]
+            result[key] = this.deepClone(obj[key])
+          }
+          return result
+        }
+        default: {
+          throw new Error("Unable to copy obj! Its type isn't supported.")
+        }
+      }
+    },
+    betwon (start, end, startNum = 0, endNum = 0) {
+      const list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', ' U', ' V', ' W', ' X', ' Y', 'Z']
+      const obj = {
+        A: 0,
+        B: 1,
+        C: 2,
+        D: 3,
+        E: 4,
+        F: 5,
+        G: 6,
+        H: 7,
+        I: 8,
+        J: 9,
+        K: 10,
+        L: 11,
+        M: 12,
+        N: 13,
+        O: 14,
+        P: 15,
+        Q: 16,
+        R: 17,
+        S: 18,
+        T: 19,
+        U: 20,
+        V: 21,
+        W: 22,
+        X: 23,
+        Y: 24,
+        Z: 25
+      }
+      return list.slice(obj[start.toUpperCase()] + startNum, obj[end.toUpperCase()] + startNum)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .show {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    background-color: #f8f8f8;
+  }
+  .title {
+    height: 20px;
+    line-height: 20px;
+    color: white;
+    background-color: deepskyblue;
+  }
+  table {
+    width: 100%;
+    font-size: 12px;
+    height: calc(100% - 20px);
+  }
+  table td {
+    text-align: center;
+  }
+  table tr:nth-child(even) {
+    background: rgb(238, 211, 210)
+  }
+  table tr:nth-child(odd) {
+    background: #FFF
+  }
+</style>
